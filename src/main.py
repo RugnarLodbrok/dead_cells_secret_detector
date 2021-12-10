84a70c996029effb7c21380601565c3df804a6df
rugnar <rugnarlodbrok@gmail.com>
2021-12-10 23:00:42 +0300

    framge generator + resize

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+import os
 from contextlib import contextmanager
-from typing import Iterator
+from typing import Iterator, Tuple, Union
 
 import win32gui
 import win32ui
@@ -10,26 +11,47 @@ import py_tools
 import time
 
 from PIL import ImageGrab
+from PIL import Image
 
 
-def capture(windowname: str = None, size=(1920, 1080)) -> Iterator[np.ndarray]:
-    w, h = size
-    dataBitMap = dcObj = cDC = wDC = hwnd = None
+def capture(window_name: str = None, size: Union[float, Tuple[int, int]] = None) -> Iterator[np.ndarray]:
+    """
+    Based on
+    https://stackoverflow.com/a/3586280/3367753
+    """
+    bmp = dcObj = cDC = wDC = hwnd = None
     try:
-        hwnd = windowname and win32gui.FindWindow(None, windowname)
+        hwnd = window_name and win32gui.FindWindow(None, window_name)
+        x0, y0, x1, y1 = win32gui.GetWindowRect(hwnd)
+        w = x1 - x0
+        h = y1 - y0
+        if isinstance(size, float):
+            size = round(w * size), round(h * size)
         wDC = win32gui.GetWindowDC(hwnd)
         dcObj = win32ui.CreateDCFromHandle(wDC)
         cDC = dcObj.CreateCompatibleDC()
-        dataBitMap = win32ui.CreateBitmap()
-        dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
-        cDC.SelectObject(dataBitMap)
         while True:
-            cDC.BitBlt((0, 0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
+            bmp = win32ui.CreateBitmap()
+            bmp.CreateCompatibleBitmap(dcObj, w, h)
+            cDC.SelectObject(bmp)
+            cDC.BitBlt(
+                (0, 0),
+                (w, h),  # width, height
+                dcObj,
+                (0, 0),  # left; top
+                win32con.SRCCOPY,
+            )
             # dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
-            data = dataBitMap.GetBitmapBits(True)
+            data = bmp.GetBitmapBits(True)
             frame = np.frombuffer(data, dtype=np.uint8)
             frame = frame.reshape([h, w, 4])
+            if size:
+                frame = cv.resize(frame, (size[0], size[1]), interpolation=cv.INTER_LINEAR)
+            frame = frame[..., : 3]
+            frame = np.ascontiguousarray(frame)
             yield frame
+            if bmp:
+                win32gui.DeleteObject(bmp.GetHandle())
 
     finally:
         # Free Resources
@@ -39,20 +61,27 @@ def capture(windowname: str = None, size=(1920, 1080)) -> Iterator[np.ndarray]:
             cDC.DeleteDC()
         if hwnd and wDC:
             win32gui.ReleaseDC(hwnd, wDC)
-        if dataBitMap:
-            win32gui.DeleteObject(dataBitMap.GetHandle())
+        if bmp:
+            win32gui.DeleteObject(bmp.GetHandle())
 
 
-if __name__ == '__main__':
+def main():
+    size = 2560, 1440
     try:
         t = time.perf_counter()
-        for frame in capture():
+        for frame in capture('Dead Cells', size=.5):
             cv.imshow('CV', frame)
             t1 = time.perf_counter()
             print(1 / (t1 - t))
             t = t1
             if cv.waitKey(1) == ord('q'):
                 break
+            # Image.fromarray(frame).show()
     finally:
         cv.destroyAllWindows()
         print('done')
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.dirname(__file__)))
+    main()
